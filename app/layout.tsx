7da2ab10988db212ca2e19105434b7b91ed52ca6
jonxxxxx <130520347+jonxxxxx@users.noreply.github.com>
2024-04-24 17:16:55 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "iBot V2.10 DONGXUAN",
+  title: "iBot v2.10",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
